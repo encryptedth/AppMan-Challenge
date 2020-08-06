54bf0c65e7f1b9541a0dca2ab64edc32d4dda416
encryptedth <36179756+encryptedth@users.noreply.github.com>
2020-08-07 00:21:17 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,9 +8,7 @@ namespace AppMan
         static void Main(string[] args)
         {
             string input = "a5b7c3";
-            int resultInt = Convert.ToInt32(Regex.Replace(input, "[^0-9]", ""));
-
-            Console.WriteLine(resultInt);
+            Console.WriteLine(Regex.Replace(input, "[^0-9]", ""));
         }
     }
 }
